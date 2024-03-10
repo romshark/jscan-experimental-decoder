f5724f12f4ec8086b0209ff2453e53b2bacac539
Roman Sharkov <roman.scharkov@gmail.com>
2024-03-10 01:13:03 +0100

    feat: Add Number methods

--- jscandec.go
+++ jscandec.go
@@ -32,18 +32,18 @@ var (
 // Number represents a JSON number literal.
 type Number string
 
-// // String returns the literal text of the number.
-// func (n Number) String() string { return string(n) }
+// String returns the literal text of the number.
+func (n Number) String() string { return string(n) }
 
-// // Float64 returns the number as a float64.
-// func (n Number) Float64() (float64, error) {
-// 	return strconv.ParseFloat(string(n), 64)
-// }
+// Float64 returns the number as a float64.
+func (n Number) Float64() (float64, error) {
+	return strconv.ParseFloat(string(n), 64)
+}
 
-// // Int64 returns the number as an int64.
-// func (n Number) Int64() (int64, error) {
-// 	return strconv.ParseInt(string(n), 10, 64)
-// }
+// Int64 returns the number as an int64.
+func (n Number) Int64() (int64, error) {
+	return strconv.ParseInt(string(n), 10, 64)
+}
 
 var tpNumber = reflect.TypeOf(Number(""))
 
